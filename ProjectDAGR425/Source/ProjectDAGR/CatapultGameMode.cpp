059f9867f20775ce1ee3ed7268a01c06a882b677
BaxterDulieu <bdulieu@finessdata.com>
2022-09-05 23:35:51 +1000

    Fixed Up Catapult Level

---
+++ ProjectDAGR425/Source/ProjectDAGR/CatapultGameMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CatapultGameMode.h"
+
+void ACatapultGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessages)
+{
+	Super::InitGame(MapName, Options, ErrorMessages);
+
+}
+
+void ACatapultGameMode::IncreaseScore() {
+
+}
