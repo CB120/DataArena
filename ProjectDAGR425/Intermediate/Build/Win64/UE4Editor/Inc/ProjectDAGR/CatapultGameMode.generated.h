3751da6aad1ae7e87e728c7eff7642e4c4281d0c
ItsFitzy <79495553+ItsFitzy@users.noreply.github.com>
2022-09-20 14:32:22 +1000

    Progress Save

---
+++ ProjectDAGR425/Intermediate/Build/Win64/UE4Editor/Inc/ProjectDAGR/CatapultGameMode.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PROJECTDAGR_CatapultGameMode_generated_h
+#error "CatapultGameMode.generated.h already included, missing '#pragma once' in CatapultGameMode.h"
+#endif
+#define PROJECTDAGR_CatapultGameMode_generated_h
+
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_SPARSE_DATA
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_RPC_WRAPPERS
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACatapultGameMode(); \
+	friend struct Z_Construct_UClass_ACatapultGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ACatapultGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ProjectDAGR"), NO_API) \
+	DECLARE_SERIALIZER(ACatapultGameMode)
+
+
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACatapultGameMode(); \
+	friend struct Z_Construct_UClass_ACatapultGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ACatapultGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ProjectDAGR"), NO_API) \
+	DECLARE_SERIALIZER(ACatapultGameMode)
+
+
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACatapultGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACatapultGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACatapultGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACatapultGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACatapultGameMode(ACatapultGameMode&&); \
+	NO_API ACatapultGameMode(const ACatapultGameMode&); \
+public:
+
+
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACatapultGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACatapultGameMode(ACatapultGameMode&&); \
+	NO_API ACatapultGameMode(const ACatapultGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACatapultGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACatapultGameMode); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACatapultGameMode)
+
+
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_PRIVATE_PROPERTY_OFFSET
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_12_PROLOG
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_PRIVATE_PROPERTY_OFFSET \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_SPARSE_DATA \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_RPC_WRAPPERS \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_INCLASS \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_PRIVATE_PROPERTY_OFFSET \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_SPARSE_DATA \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_INCLASS_NO_PURE_DECLS \
+	ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PROJECTDAGR_API UClass* StaticClass<class ACatapultGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ProjectDAGR425_Source_ProjectDAGR_CatapultGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
